c1c29376ab43b5e98d666b787ab17b44bd0facb7
huangyaping <huangyp1991@gmail.com>
2012-12-28 14:37:33 +0800

    0.1

--- tetris/tries.py
+++ tetris/tries.py
@@ -1,10 +1,31 @@
+import threading
 
 def main():
-    global screen
-    screen = 1
-    init()
+    global cnt, isdead, lock
+    cnt = 0
+    isdead = False
+    lock = threading.RLock()
+    newThread(1)
+    newThread(2)
 
-def init():
-    print screen
+def newThread(tid):
+    t = threading.Thread(target=doit, name=tid)
+    t.deamon = True
+    t.start()
+def doit():
+    while not isdead:
+        incr()
+def incr():
+    global cnt, isdead, lock
+
+    with lock:
+        for i in range(100*2):
+            cnt += 1
+    #    print cnt
+        if cnt % 2 != 0:
+            print 'Confilct occurs between threads.'
+            print 'Current value:', cnt
+            isdead = True
 
 main()
+
